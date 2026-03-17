966cce8096c27bb98aed7281fe4c33b676bf46b4
ANWINBABY <143954206+ANWINBABY@users.noreply.github.com>
2026-03-17 19:31:43 +0530

    Update wsgi.py

--- note_management/note_management/wsgi.py
+++ note_management/note_management/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_management.settings')
-
+# Correct module path
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_management.note_management.settings')
 application = get_wsgi_application()
